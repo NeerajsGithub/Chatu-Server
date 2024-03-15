ed175a37ce83ab5b78b417ec6b962307b6ecbefc
Niraj <neerajnaphade02@gmail.com>
2024-03-15 18:56:34 +0530

    Mongo Changed

--- data/page.js
+++ data/page.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export function connectDb () {
 
-    mongoose.connect(process.env.MONGO_URL);
+    mongoose.connect('mongodb+srv://chatu890:chatu890@chatu.avmwqdc.mongodb.net/?retryWrites=true&w=majority&appName=Chatu' || process.env.MONGO_URL);
     const db = mongoose.connection;
     db.on('error', (error) => console.error('MongoDB connection error:', error));
     db.once('open', () => console.log('Connected to MongoDB'));
